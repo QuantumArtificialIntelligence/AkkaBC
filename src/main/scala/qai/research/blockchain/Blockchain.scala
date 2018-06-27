21e60e9ced293dde6b38729eb4633736fe02a39d
antidata <mariano@quantumintelligence.ai>
2018-06-26 22:40:42 -0400

    Moving init to BlockApp

--- src/main/scala/qai/research/blockchain/Blockchain.scala
+++ src/main/scala/qai/research/blockchain/Blockchain.scala
@@ -35,14 +35,14 @@ class Blockchain(blockchainId: String) extends Actor with ActorLogging {
     case SetPostValidator(postV) =>
       postValidator = Some(postV)
 
-    case addBlock @ AddBlock(block) =>
-      preValidator.map(_ ! addBlock).getOrElse(self ! PreValidatedBlock(block))
+    case addBlock @ AddBlock(block, previousHash) =>
+      preValidator.map(_ ! addBlock).getOrElse(self ! PreValidatedBlock(block, previousHash))
 
-    case preValidatedBlock @ PreValidatedBlock(block) =>
-      preProcessStrategy.map(_ ! preValidatedBlock).getOrElse(self ! ProcessedBlock(block))
+    case preValidatedBlock @ PreValidatedBlock(block, previousHash) =>
+      preProcessStrategy.map(_ ! preValidatedBlock).getOrElse(self ! ProcessedBlock(block, previousHash))
 
-    case processedBlock @ ProcessedBlock(block) =>
-      postValidator.map(_ ! processedBlock).getOrElse(self ! PostValidatedBlock(block, getBlockInfo(block).lastHash))
+    case processedBlock @ ProcessedBlock(block, previousHash) =>
+      postValidator.map(_ ! processedBlock).getOrElse(self ! PostValidatedBlock(block, previousHash))
 
     case PostValidatedBlock(block, blockLastHash) if blockLastHash == lastHash =>
       blocks = block :: blocks
@@ -75,32 +75,10 @@ object Blockchain {
   case object GetLastBlockHash
   case object PrintBlockchain
 
-  case class AddBlock(block: BlockRef)
-  case class PreValidatedBlock(block: BlockRef)
-  case class ProcessedBlock(block: BlockRef)
-  case class PostValidatedBlock(block: BlockRef, lastHash: String)
-
-  var currentBlockchain: Option[ActorRef] = None
-  var actorSystem: Option[ActorSystem] = None
-
-  def main(args: Array[String]): Unit = {
-
-    // Override the configuration of the port when specified as program argument
-    val port = if (args.isEmpty) "0" else args(0)
-
-    val config = ConfigFactory.parseString(s"akka.remote.netty.tcp.port=$port").
-      withFallback(ConfigFactory.parseString("akka.cluster.roles = [blockchain]")).
-      withFallback(ConfigFactory.load("akkablockchain"))
-
-    val system = ActorSystem("ClusterSystem", config)
-    actorSystem = Some(system)
-
-    currentBlockchain = Some(system.actorOf(Props(new Blockchain("ActorsSuperBlockchain")), name = "blockchainActor"))
-
-    Blocks.currentBlocks = Some(system.actorOf(Props(new Blocks(Blockchain.currentBlockchain.get))))
-
-    system.actorOf(Props[MetricsListener], name = "metricsListener")
-  }
+  case class AddBlock(block: BlockRef, previousHash: String)
+  case class PreValidatedBlock(block: BlockRef, previousHash: String)
+  case class ProcessedBlock(block: BlockRef, previousHash: String)
+  case class PostValidatedBlock(block: BlockRef, previousHash: String)
 
   def getCurrentTimestamp: Long = (new Date).getTime
 
@@ -110,6 +88,8 @@ object Blockchain {
 
   def getBlockInfo(block: BlockRef): BasicBlock.BlockInfo = {
     import java.util.concurrent.TimeUnit
+    implicit  val timeout: akka.util.Timeout = akka.util.Timeout(2000, TimeUnit.MILLISECONDS)
+
     Await.result(((block ? GetBlockInfo).mapTo[BasicBlock.BlockInfo]), scala.concurrent.duration.Duration(2000, TimeUnit.MILLISECONDS))
   }
 }
